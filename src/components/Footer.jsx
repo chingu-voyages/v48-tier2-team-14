fb42e2ffdbe7a12c2239a647f9faa33151d5a44b
Jonah Unuafe <fejisfejiro@gmail.com>
2024-03-11 11:34:19 +0100

    added Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <>
+            <ul>
+                <li>
+                    <a href="https://github.com/chingu-voyages/v48-tier2-team-14">Team-14</a>
+                </li>
+            </ul>
+        </>
+    );
+}
+
+export default Footer;
