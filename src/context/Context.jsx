a5bc1809fdb4e7c1ebb2defacf9f9e845653fdfb
Simon Summers <simon7summers@gmail.com>
2024-03-11 23:11:12 +0000

    added halfData const in context to test if charts are working as expected

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState, useEffect } from "react";
-import { getFetchData } from '../global/utils.js'
-import LoadingPage from '../components/LoadingPage'
+import { getFetchData } from "../global/utils.js";
+import LoadingPage from "../components/LoadingPage";
 
 const AppContext = createContext();
 const DINO_API_URL = "https://chinguapi.onrender.com/dinosaurs";
@@ -10,38 +10,33 @@ const AppProvider = ({ children }) => {
 	const [data, setData] = useState(0);
 	const [loading, setLoading] = useState(true);
 
-useEffect(() => {
-	const fetchData = async () => {
-		try {
-			const responseData = await getFetchData(DINO_API_URL);
-			setData(responseData);
-			setTimeout(() => {
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const responseData = await getFetchData(DINO_API_URL);
+				const halfData = responseData.slice(0, responseData.length / 2);
+				setData(responseData);
+				setTimeout(() => {
+					setLoading(false);
+				}, 2000); // Set loading page duration to 2 seconds
+			} catch (error) {
+				console.error("Error fetching data:", error);
 				setLoading(false);
-			}, 2000); // Set loading page duration to 2 seconds
-		} catch (error) {
-			console.error("Error fetching data:", error);
-			setLoading(false); 
-		}
-	};
+			}
+		};
 
-	fetchData();
-}, []);
+		fetchData();
+	}, []);
 
 	if (loading) {
 		return (
 			<div className="ldrs">
-
 				<LoadingPage isLoading={loading} />
 			</div>
-		)
+		);
 	}
 
-	return (
-		<AppContext.Provider value={{ data }}>
-			{children}
-		</AppContext.Provider>
-	);
+	return <AppContext.Provider value={{ data }}>{children}</AppContext.Provider>;
 };
 
 export { AppContext, AppProvider };
-
