130022de4c238ea5ab04263022d440dd69a75dd5
Jonah Unuafe <fejisfejiro@gmail.com>
2024-03-16 22:34:50 +0100

    bootstrap imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+//Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+//Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.js";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
