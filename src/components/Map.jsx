3befac81b76e3f8c4d413cf4c1250e32ca3e3aeb
Rhys Miller <rjmills87@aol.com>
2024-04-01 21:36:26 +0100

    feat:Implemented Marker pop up window functionality to Map feature

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,14 @@
-import { APIProvider, Map, AdvancedMarker } from "@vis.gl/react-google-maps";
+import {
+  APIProvider,
+  Map as DinoMap,
+  AdvancedMarker,
+  Pin,
+  InfoWindow,
+  useAdvancedMarkerRef,
+} from "@vis.gl/react-google-maps";
 // import { MarkerClusterer } from "@googlemaps/markerclusterer";
 // import { Marker } from "@googlemaps/markerclusterer";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../context/Context";
 
 function Map() {
@@ -9,20 +16,58 @@ function Map() {
   const mapId = import.meta.env.VITE_GOOGLE_MAPS_MAP_ID;
   const zoom = 2;
   const mapCenter = { lat: 0, lng: 0 };
+  const [markerRef, marker] = useAdvancedMarkerRef();
+  const [selectedDinosaur, setSelectedDinosaur] = useState(null);
+  const [openPopup, setOpenPopup] = useState(false);
   const { locationCoordinates } = useContext(AppContext);
+
+  const handleMarkerClick = (dinosaur) => {
+    if (selectedDinosaur === dinosaur) {
+      setOpenPopup(!openPopup);
+    } else {
+      setSelectedDinosaur(dinosaur);
+      setOpenPopup(true);
+    }
+  };
+
+  const handleWindowClose = () => {
+    setOpenPopup(null);
+  };
 
   return (
     <APIProvider apiKey={apiKey}>
       <div style={{ height: "700px" }}>
-        <Map zoom={zoom} center={mapCenter} mapId={mapId}>
+        <DinoMap zoom={zoom} center={mapCenter} mapId={mapId}>
           {locationCoordinates.map(({ dinosaur, coordinates }) =>
             coordinates.map((coordinate, index) => (
-              <AdvancedMarker position={coordinate} key={index}>
-                <span></span>
+              <AdvancedMarker
+                ref={markerRef}
+                position={coordinate}
+                key={index}
+                onClick={() => handleMarkerClick(dinosaur)}
+              >
+                <Pin />
               </AdvancedMarker>
             ))
           )}
-        </Map>
+          {openPopup && selectedDinosaur && (
+            <InfoWindow
+              anchor={marker}
+              maxWidth="100%"
+              height="auto"
+              onCloseClick={() => handleWindowClose()}
+            >
+              <h3>{selectedDinosaur.name}</h3>
+              <div className="img-container">
+                <img
+                  className="popup-img"
+                  src={selectedDinosaur.imageSrc}
+                  alt={selectedDinosaur.name}
+                />
+              </div>
+            </InfoWindow>
+          )}
+        </DinoMap>
       </div>
     </APIProvider>
   );
