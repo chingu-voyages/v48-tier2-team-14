e7017f917d9c3b1b769d3e81f6284a7a77fa568d
Jonah Unuafe <fejisfejiro@gmail.com>
2024-04-05 12:43:51 +0100

    refactor Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,16 @@ export default function Navbar() {
   return (
     <div className="row mt-3 mb-2">
         <div className="col-md-8">
-            <h3>Dino Studio</h3>
+          <h3>Dino Studio</h3>
         </div>
+        
         <div className="col-md-4 d-inline-flex justify-content-around mt-1 fw-bold cursor-pointer">
-            <a href="#">Search dinosaur</a>
-            <a href="#">News</a>
-            <a href="#">Connect Us</a>
+            <a>Search dinosaur</a>
+            <a>News</a>
+            <a>Connect Us</a>
         </div>
-	</div>
+    </div>
   );
 }
+
+
