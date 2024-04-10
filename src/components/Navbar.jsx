528cabe10ce34fa434df495598e008df8f83dfa8
Jonah Unuafe <fejisfejiro@gmail.com>
2024-04-10 11:31:26 +0100

    modify Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 export default function Navbar() {
   return (
     <div className="row mt-3 mb-2">
-        <div className="col-md-8">
-          <h3>Dino Studio</h3>
-        </div>
+      <div className="col-md-8">
+        <h3>Dino Studio</h3>
+      </div>
 
-        <div className="col-md-4 d-inline-flex justify-content-around mt-1 fw-bold cursor-pointer">
-            <a>Search dinosaur</a>
-            <a>News</a>
-            <a>Connect Us</a>
-        </div>
+      <div className="col-md-4 d-inline-flex justify-content-around mt-1 fw-medium cursor-pointer">
+        <a href="#" className="text-decoration-none text-black">Search dinosaur</a>
+        <a href="#" className="text-decoration-none text-black">News</a>
+        <a href="#" className="text-decoration-none text-black">Connect Us</a>
+      </div>
     </div>
   );
 }
