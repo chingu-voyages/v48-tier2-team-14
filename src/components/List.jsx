0ee45074b7c0383e04d96ba761c70b917533b468
simonC137 <simon7summers@gmail.com>
2024-04-13 18:26:02 +0100

    fix dino-detail top border

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -26,7 +26,7 @@ function List() {
 		<>
 			<div className="headerText">
 				<h6 className="display-12 text-uppercase w-50 mx-1 text-center py-1">
-					dinosaur lists
+					dinosaur list
 				</h6>
 			</div>
 			<div className="dinoItemList mt-4">
