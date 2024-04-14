b526baec63bc23587ea5a46ebf33ec168fa454db
Joshf225 <joshuafamose@gmail.com>
2024-04-14 14:53:25 +0100

    hamburger functionality fix, completed mobile responsiveness

--- src/components/DinosaurNews.jsx
+++ src/components/DinosaurNews.jsx
@@ -7,82 +7,82 @@ export default function DinosaurNews() {
   const [btnText, setBtnText] = useState("View all");
 
   return (
-		<div className="news-container mt-5">
-			<div id="styled-container">
-				<h2 className="news-heading mt-4">Latest News</h2>
-				<p className="news-intro mb-4 text-center">
-					Stay up to date with exciting discoveries, scientific breakthroughs,
-					and fascinating insights into these prehistoric creatures.
-				</p>
-				<div className="articles">
-					{dinoNews &&
-						dinoNews
-							.slice(0, viewAll ? dinoNews.length : 3)
-							.map((article, index) => {
-								const publishedAt = new Date(article.publishedAt);
-								const options = {
-									year: "numeric",
-									month: "long",
-									day: "numeric",
-								};
+    <div className="news-container mt-5" id="dinoNews">
+      <div id="styled-container">
+        <h2 className="news-heading mt-4">Latest News</h2>
+        <p className="news-intro mb-4 text-center">
+          Stay up to date with exciting discoveries, scientific breakthroughs,
+          and fascinating insights into these prehistoric creatures.
+        </p>
+        <div className="articles">
+          {dinoNews &&
+            dinoNews
+              .slice(0, viewAll ? dinoNews.length : 3)
+              .map((article, index) => {
+                const publishedAt = new Date(article.publishedAt);
+                const options = {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                };
 
-								const formattedDate = publishedAt.toLocaleString(
-									"en-GB",
-									options
-								);
+                const formattedDate = publishedAt.toLocaleString(
+                  "en-GB",
+                  options
+                );
 
-								return (
-									<div className="news-card" key={index}>
-										<div className="card-img-container">
-											<img
-												className="article-img"
-												loading="lazy"
-												src={article.image}
-												alt={article.title}
-											/>
-										</div>
-										<div className="card-text-container bg-white">
-											<div className="source-container">
-												<p className="article-published">{formattedDate}</p>
-												<p className="article-source">
-													By {article.source.name}
-												</p>
-											</div>
-											<h3 className="article-title">{article.title}</h3>
-											<p className="article-description">
-												{article.description}
-											</p>
-											<div className="article-link-container">
-												<a
-													className="article-link"
-													href={article.url}
-													target="_blank"
-													rel="noopener noreferrer"
-												>
-													Read More
-												</a>
-												<i className="bi bi-chevron-right"></i>
-											</div>
-										</div>
-									</div>
-								);
-							})}
-				</div>
-				<button
-					onClick={() => {
-						if (!viewAll) {
-							setViewAll(true);
-							setBtnText("Close");
-						} else {
-							setViewAll(false);
-							setBtnText("View all");
-						}
-					}}
-					className="view-all"
-				>
-					{btnText}
-				</button>
-			</div>
-		</div>
-	);
+                return (
+                  <div className="news-card" key={index}>
+                    <div className="card-img-container">
+                      <img
+                        className="article-img"
+                        loading="lazy"
+                        src={article.image}
+                        alt={article.title}
+                      />
+                    </div>
+                    <div className="card-text-container bg-white">
+                      <div className="source-container">
+                        <p className="article-published">{formattedDate}</p>
+                        <p className="article-source">
+                          By {article.source.name}
+                        </p>
+                      </div>
+                      <h3 className="article-title">{article.title}</h3>
+                      <p className="article-description">
+                        {article.description}
+                      </p>
+                      <div className="article-link-container">
+                        <a
+                          className="article-link"
+                          href={article.url}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Read More
+                        </a>
+                        <i className="bi bi-chevron-right"></i>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+        </div>
+        <button
+          onClick={() => {
+            if (!viewAll) {
+              setViewAll(true);
+              setBtnText("Close");
+            } else {
+              setViewAll(false);
+              setBtnText("View all");
+            }
+          }}
+          className="view-all"
+        >
+          {btnText}
+        </button>
+      </div>
+    </div>
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import list from "../../assets/align-justify.svg";
 
 export default function Navbar() {
+  const [active, setActive] = useState(true);
+  const handleClick = () => {
+    setActive(!active);
+  };
   return (
     <div className="row mt-3 mb-2">
       <div className="col-md-8">
         <h3>Dino Studio</h3>
       </div>
 
-
       <div
         className="col-md-4 d-inline-flex justify-content-between mt-1 fw-medium cursor-pointer"
         id="navbar"
       >
-        <img src={list} alt="list" id="hamburger-sign" />
-        <a
-          href="#search"
-          className="text-decoration-none text-black"
-          id="search"
-        >
-          Dinosaur Search
-        </a>
-        <a href="#dinoNews" className="text-decoration-none text-black" id="news">
-          News
-        </a>
-        <a href="#footer" className="text-decoration-none text-black" id="contact">
-          Contact Us
-        </a>
-
+        <img src={list} alt="list" id="hamburger-sign" onClick={handleClick} />
+        <div className="links">
+          <a
+            href="#search"
+            className="text-decoration-none text-black"
+            id={active ? "search" : "hide"}
+          >
+            Dinosaur Search
+          </a>
+          <a
+            href="#dinoNews"
+            className="text-decoration-none text-black"
+            id={active ? "news" : "hide"}
+          >
+            News
+          </a>
+          <a
+            href="#footer"
+            className="text-decoration-none text-black"
+            id={active ? "contact" : "hide"}
+          >
+            Contact Us
+          </a>
+        </div>
       </div>
       <div id="hamburger-sign"></div>
     </div>
